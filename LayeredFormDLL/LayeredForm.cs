947db493ba78b4327a08db86361eeeff230fa0b9
vip00112 <vip00112@naver.com>
2016-12-17 19:37:58 +0900

    First adds

---
+++ LayeredFormDLL/LayeredForm.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace LayeredFormDLL {
+    public partial class LayeredForm : Form {
+        private string _caption; // 윈도우 타이틀(TITLE)
+        private Point _pos; // Form Move를 위한 현재 좌표 저장
+
+        public LayeredForm() {
+            _caption = "TITLE";
+            _pos = new Point();
+
+            FormBorderStyle = FormBorderStyle.None;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        // 윈도우 타이틀(CAPTION) 지정
+        public void setCaption(string caption) {
+            _caption = caption;
+        }
+
+        #region Override Methods
+        // LAYERED 스타일 반영
+        protected override CreateParams CreateParams {
+            get {
+                CreateParams createParams = base.CreateParams;
+                if (!DesignMode) {
+                    createParams.ExStyle |= WS_EX_LAYERED;
+                }
+                return createParams;
+            }
+        }
+
+        // Form Move : 마우스 좌 클릭 후 이동
+        protected override void OnMouseMove(MouseEventArgs e) {
+            base.OnMouseMove(e);
+            if (e.Button == MouseButtons.Left) {
+                Location = new Point(Location.X + (_pos.X + e.X), Location.Y + (_pos.Y + e.Y));
+            }
+        }
+
+        // Form Move : 마우스 좌 땠을시 위치 확정
+        protected override void OnMouseDown(MouseEventArgs e) {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left) {
+                _pos = new Point(-e.X, -e.Y);
+            }
+        }
+
+        // 마우스 더블 클릭 : 종료
+        protected override void OnMouseDoubleClick(MouseEventArgs e) {
+            base.OnMouseDoubleClick(e);
+            if (MessageBox.Show("정말 종료 하시겠습니까 ?", _caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                Close();
+            }
+        }
+        #endregion
+
+        #region Set Bitmap
+        protected void SelectBitmap(Bitmap bitmap) {
+            SelectBitmap(bitmap, 255);
+        }
+
+        private void SelectBitmap(Bitmap bitmap, int opacity) {
+            // Does this bitmap contain an alpha channel?
+            if (bitmap.PixelFormat != PixelFormat.Format32bppArgb) {
+                throw new ApplicationException("The bitmap must be 32bpp with alpha-channel.");
+            }
+
+            // Get device contexts
+            IntPtr screenDc = GetDC(IntPtr.Zero);
+            IntPtr memDc = CreateCompatibleDC(screenDc);
+            IntPtr hBitmap = IntPtr.Zero;
+            IntPtr hOldBitmap = IntPtr.Zero;
+
+            try {
+                // Get handle to the new bitmap and select it into the current device context.
+                hBitmap = bitmap.GetHbitmap(Color.FromArgb(0));
+                hOldBitmap = SelectObject(memDc, hBitmap);
+
+                // Set parameters for layered window update.
+                MyPoint locationInWindow = new MyPoint(Left - (bitmap.Width / 2), Top - (bitmap.Height / 2));
+                MySize sizeInWindow = new MySize(bitmap.Width, bitmap.Height);
+                MyPoint LocationInDC = new MyPoint(0, 0);
+                BLENDFUNCTION blend = new BLENDFUNCTION();
+                blend.BlendOp = AC_SRC_OVER;
+                blend.BlendFlags = 0;
+                blend.SourceConstantAlpha = (byte) opacity;
+                blend.AlphaFormat = AC_SRC_ALPHA;
+
+                // Update the window.
+                UpdateLayeredWindow(
+                    this.Handle,     // Handle to the layered window
+                    screenDc,        // Handle to the screen DC
+                    ref locationInWindow, // New screen position of the layered window
+                    ref sizeInWindow,     // New size of the layered window
+                    memDc,           // Handle to the layered window surface DC
+                    ref LocationInDC, // Location of the layer in the DC
+                    0,               // Color key of the layered window
+                    ref blend,       // Transparency of the layered window
+                    ULW_ALPHA        // Use blend as the blend function
+                    );
+            } finally {
+                // Release device context.
+                ReleaseDC(IntPtr.Zero, screenDc);
+                if (hBitmap != IntPtr.Zero) {
+                    SelectObject(memDc, hOldBitmap);
+                    DeleteObject(hBitmap);
+                }
+                DeleteDC(memDc);
+            }
+        }
+        #endregion
+
+        #region Native Methods and Structures
+        private const int WS_EX_LAYERED = 0x80000;
+        private const int HTCAPTION = 0x02;
+        private const int WM_NCHITTEST = 0x84;
+        private const int ULW_ALPHA = 0x02;
+        private const byte AC_SRC_OVER = 0x00;
+        private const byte AC_SRC_ALPHA = 0x01;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MyPoint {
+            public int x;
+            public int y;
+
+            public MyPoint(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MySize {
+            public int cx;
+            public int cy;
+
+            public MySize(int cx, int cy) {
+                this.cx = cx;
+                this.cy = cy;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct ARGB {
+            public byte Blue;
+            public byte Green;
+            public byte Red;
+            public byte Alpha;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct BLENDFUNCTION {
+            public byte BlendOp;
+            public byte BlendFlags;
+            public byte SourceConstantAlpha;
+            public byte AlphaFormat;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UpdateLayeredWindow(IntPtr hwnd, IntPtr hdcDst,
+            ref MyPoint pptDst, ref MySize psize, IntPtr hdcSrc, ref MyPoint pprSrc,
+            int crKey, ref BLENDFUNCTION pblend, int dwFlags);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteObject(IntPtr hObject);
+        #endregion
+    }
+}
---
+++ README.txt
@@ -0,0 +1,56 @@
+﻿C# Layered Window 생성 DLL
+개발 툴 : Microsoft Visual Studio 15
+대상 프레임워크 : .NET Framework 4 Client Profile
+
+1. 설명
+ 32비트 투명 배경을 가지는 png 파일을 이용하여 Layerd Form을 그려준다.
+ MSDN의 예제파일을 사용하기 쉽도록 dll화 한 프로젝트 이다.
+ System.Windows.Forms.Form class를 상속받아 LayeredForm class를 구현 했다.
+
+2. 내장 Override 메서드
+  1) CreateParams : LAYERED 스타일 반영
+  2) OnMouseMove,OnMouseDown : Form의 위치 이동 지원
+  3) OnMouseDoubleClick : Form 종료
+
+3. 사용 예제
+  1. Windows Forms Application Project 생성
+  2. 기본 생성된 Form class를 InitForm으로 정정
+  3. 실제 LayerdForm이 될 MainForm 생성
+    1) System.Windows.Forms.Form이 아닌 LayeredForm을 상속 받는다.
+    2) Load 이벤트를 등록 후 LayeredForm의 protected 메서드인 SelectBitmap를 호출 한다.
+  4. InitForm에 Load 이벤트를 등록 후 MainForm을 생성 한다.
+
+    // InitForm 소스 코드
+    public partial class InitForm : Form {
+        public InitForm() {
+            InitializeComponent();
+
+            // InitForm 숨김
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+
+            // MainForm 표기
+            MainForm form = new MainForm();
+            form.FormClosing += InitFormClosing;
+            form.Show();
+        }
+
+        // MainForm 종료시 기본 Form인 InitForm을 종료
+        private void InitFormClosing(object sender, FormClosingEventArgs e) {
+            Close();
+        }
+    }
+
+    // MainForm 소스 코드
+    public partial class MainForm : LayeredForm {
+        public MainForm() {
+            InitializeComponent();
+
+	    // Title 지정
+            setCaption("MainForm");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            SelectBitmap(Properties.Resources.BackgroundImg.png);
+        }
+    }
